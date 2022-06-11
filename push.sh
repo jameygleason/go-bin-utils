a4bc7a792a23e6c97d498e8cbb59414cf56b84b8
jameygleason <james.p.gleason@gmail.com>
2022-06-11 06:38:09 -0400

    added push script

---
+++ push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+git remote set-url origin git@github.com:jameygleason/go-bin-utils.git
+git push origin
+git remote set-url origin git@gitlab.com:jameygleason/go-bin-utils.git
+git push origin
